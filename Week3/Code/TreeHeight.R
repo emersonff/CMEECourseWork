77741d3e189f53ee623e7d421dfe91dc78a07765
xiang li <xiang.li419@imperial.ac.uk>
2019-10-16 22:29:20 +0100

    Added Week3 practical 1

--- Week3/Code/TreeHeight.R
+++ Week3/Code/TreeHeight.R
@@ -9,21 +9,35 @@
 #
 # OUTPUT
 # The heights of the tree, same units as "distance"
+##############
+#Loads trees.csv and calculates tree heights for all trees in the data. 
+#Note that the distances have been measured in meters. (Hint: use relative paths)).
+#
+#Creates a csv output file called TreeHts.csv in results that contains the calculated 
+#tree heights along with the original data 
+
+#apply() -operates on array/matrix
+#lpply() -can be used for other objects and output is a list
+#spply() -similar to lpply but try to output the most elementary data structure that is possible
+
 
 ###functions
-TreeHeight <- function(data){ #data[1]:species    data[2]:distances in meter    data[3]: degrees
-  for(i in data){}
-  radians <- degrees * pi /180
-  height <-distance * tan(radians)
+TreeHeight <- function(m){ #m[1]:species    m[2]:distances in meter    m[3]: degrees
+  radians <- as.numeric(m[3]) * pi /180 ##coerce from character to numeric type for calculation
+  height <-as.numeric(m[2]) * tan(radians)
   print(paste("The tree heightis:", height))
-  
-  return (height)
+  return (height) #return a vector of heights
 }
 
 
 
-datax <- read.csv("../Data/trees.csv")
-TreeHeight(37, 40)
+trees.df <- read.csv("../Data/trees.csv") # data.frame
+trees.m <- as.matrix(trees.df)#matrix using matrix instead of data.frame because it is faster when lager number of mathematical 
+                              #calculations
+
+trees.df$Tree.Height.m <-apply(trees.m, 1, TreeHeight) #apply TreeHeight for each row of trees.m and add output vector to trees
+                                                       #data.fram as a new column named Tree.Height.m
+write.csv(file = "../Results/TreeHts.csv", trees.df) #write results
 
 
 
